bb68dc9a6f57b9b9efc920c8967f5088e4676cd3
Max Henning Junghans <MaxHenningJunghans@gmail.com>
2023-03-06 12:15:08 +0100

    Added an input to ask user for block data

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,28 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
-
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
     val blockchain = Blockchain(args[0])
     blockchain.printBlockchain()
+    val test = askUserForData()
+}
+
+fun askUserForData(): String {
+    println("Write the data you would like to add.")
+    return readln()
+}
+
+fun askUserForDataWithLinebreaks(): String{
+    println("Write the data you would like to add. If you are done, write !end")
+    var data = ""
+    while (true) {
+        val input = readln()
+        if (input == "!end") {
+            break
+        }
+        data += input + System.lineSeparator()
+    }
+
+    data = data.substringBefore(System.lineSeparator())
+    return data
 }
